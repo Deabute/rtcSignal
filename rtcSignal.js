2d1c3bc999c0e2b8a8d9cd6be1e253d53f70d4b6
PaulBeaudet <inof8or@gmail.com>
2019-01-20 16:16:34 -0500

    basic call ending

--- rtcSignal.js
+++ rtcSignal.js
@@ -3,61 +3,71 @@ var crypto = require('crypto');
 
 var user = { // definitely use a database for this
     s: [],   // array of connected clients, so long as server is up
-    connect:  function(peer, me, offer){ // find a specific peer to connect with
-        for(var i = 0; i < user.s.length; i++){
-            if(user.s[i].id === peer && !user.s[i].con){   // if peer id matches a socket that is connected
-                user.s[i].con = true;                      // set user as connected to someone
-                user.s[i].ws.send(JSON.stringify({type: 'offer', from: me, offer: offer}));
-                return true;
+    connect:  function(wsID, offer, peerID){               // find a specific peer to connect with
+        if(peerID){
+            for(var i = 0; i < user.s.length; i++){
+                if(!user.s[i].con && peerID === user.s[i].id){
+                    user.s[i].con = wsID;                      // note who this peer is about to be connected to
+                    user.s[i].ws.send(JSON.stringify({type: 'offer', from: wsID, offer: offer}));
+                    return true;                               // confirm match was made
+                }
+            }
+        } else {
+            for(var j = 0; j < user.s.length; j++){
+                if(!user.s[j].con && user.s[j].id !== wsID){   // if peer id matches a socket that is connected and they are not engadges in a connection
+                    user.s[j].con = wsID;                      // note who this peer is about to be connected to
+                    user.s[j].ws.send(JSON.stringify({type: 'offer', from: wsID, offer: offer}));
+                    return true;
+                }
             }
         }
-        return false;
+        return false; // note if no match was made
     },
-    rando:  function(id){ // find a specific peer to connect with
+    endChat: function(wsID){
         for(var i = 0; i < user.s.length; i++){
-            if(user.s[i].id !== id && !user.s[i].con){return user.s[i].id;}
+            if(user.s[i].id === wsID){          // find wsID
+                console.log('disconnecting:' + wsID);
+                user.s[i].con = '';             // remove connection from requesters obj
+            } else if (user.s[i].con === wsID){ // find who is connected to wsID
+                console.log('from: ' + user.s[i].id);
+                user.s[i].con = '';             // remove connection from their peer to free them up
+            }
         }
-        return 0;
-    }
-};
-
-var signal = {
-    token: function(onToken){ // "its so random colisions are unlikely" sounds alot like optimism
-        crypto.randomBytes(8, function onRandom(err, buffer){onToken(buffer.toString('hex'));});
     }
 };
 
 var socket = {
     server: null,
+    id: function(onToken){ // "its so random colisions are unlikely" sounds alot like optimism
+        crypto.randomBytes(8, function onRandom(err, buffer){onToken(buffer.toString('hex'));});
+    },
     init: function(port){
         var WebSocket = require('ws');
         socket.server = new WebSocket.Server({ port: port });
         socket.server.on('connection', function connection(ws) {
-            signal.token(function(token){
-                user.s.push({ws: ws, id: token, con: false});         // on token create a user
-                ws.send(JSON.stringify({type:'token', data: token})); // show client what their id is so that they can share it
+            socket.id(function(wsID){
+                user.s.push({ws: ws, id: wsID, con: false});         // on token create a user
+                ws.send(JSON.stringify({type:'token', data: wsID})); // show client what their id is so that they can share it
+                ws.on('message', function incoming(message) {             // handle incoming request
+                    var res = socket.incoming(wsID, message);
+                    if(res.type){ws.send(JSON.stringify(res));}           // given default response object was manipulated respond to client
+                });
             });
-            ws.on('message', function incoming(message) {             // handle incoming request
-                var res = socket.incoming(message);
-                if(res.type){ws.send(JSON.stringify(res));}           // given default response object was manipulated respond to client
-            });
-
         });
     },
-    incoming: function(message){
+    incoming: function(wsID, message){
         var req = {type: null};                              // defaut request assumption
         try{req = JSON.parse(message);} catch(error){console.log(error);}       // try to parse JSON if its JSON if not we have a default object
         var res = {type: null};                              // default response
         if(req.type === 'offer'){                            // case where sdp and ice canidate are being traded
-            if(user.connect(req.friend, req.me, req.offer)){ // given this is a legitimate connection to make
+            // console.log(message);
+            if(user.connect(wsID, req.offer, req.friend)){   // given this is a legitimate connection to make
                 res.type = 'match';                          // ack match
             } else {res.type = 'nomatch';}                   // ack no match was founds so client can do something different
-        } else if(req.type === 'rando'){
-            res.peer = user.rando(req.id);
-            if(!res.peer){console.log('no peer?');}
-            res.type = 'rando';
+        } else if(req.type === 'disconnect'){
+            user.endChat(wsID);
         } else {
-            console.log('not JSON: ' + message);                            // given message was just a string or something other than JSON
+            console.log('thats a wooper: ' + message);       // given message was just a string or something other than JSON
         }
         return res;                                          // change default res object to respond to client
     }
